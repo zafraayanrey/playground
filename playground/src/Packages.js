c9208265d43a0df9f6ef878680d6449365be338f
Ian Rey Zafra <zafraayanrey@gmail.com>
2024-10-24 15:38:16 +0800

    v

--- playground/src/Packages.js
+++ playground/src/Packages.js
@@ -1,5 +1,59 @@
 import React from "react";
 
+const packages = [
+  {
+    id: 1,
+    packageName: "Glee",
+    price: "6500",
+    image: "../packages/1.jpg",
+    inclusions: "Glee",
+  },
+  {
+    id: 2,
+    packageName: "Happy w/ Balloon Dome",
+    price: "9500",
+    image: "../packages/2.jpg",
+    inclusions: "Happy w/ Balloon Dome",
+  },
+  {
+    id: 3,
+    packageName: "Happy",
+    price: "6500",
+    image: "../packages/3.jpg",
+    inclusions: "Happy",
+  },
+  {
+    id: 4,
+    packageName: "Jolly",
+    price: "6500",
+    image: "../packages/4.jpg",
+    inclusions: "Jolly",
+  },
+  {
+    id: 5,
+    packageName: "Jumpy Bounce",
+    price: "9500",
+    image: "../packages/5.jpg",
+    inclusions: "Jumpy Bounce",
+  },
+  {
+    id: 6,
+    packageName: "Merry",
+    price: "6500",
+    image: "../packages/6.jpg",
+    inclusions: "Merry",
+  },
+  {
+    id: 7,
+    packageName: "Rainbow",
+    price: "6500",
+    image: "../packages/7.jpg",
+    inclusions: "Rainbow",
+  },
+];
+
+packages.map((data) => console.log(data));
+
 function Packages() {
   return (
     <div className="packages">
